35d657273d404149e0c60a6569c0096862f9956c
Pix <jvall@shaw.ca>
2021-12-11 22:12:27 -0600

    updated hello line

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace jello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Jello Squirrels!");
         }
     }
 }
